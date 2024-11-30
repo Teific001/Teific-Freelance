1e0ee05fdfc0a14f88b0b8002eb93106bca3b32b
Varun Gupta <varungupta1228@gmail.com>
2024-11-30 23:49:42 +0530

    added more func

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -3,11 +3,11 @@ import "./main.css";
 import { ReactComponent as Mouse } from "../../Assets/images/magic-mouse_1.svg";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import SplitType from 'split-type'
+import SplitType from "split-type";
 import EVO from "../../Assets/images/Group 1000000914.png";
 import Header from "../Header";
-import HeroBg1 from '../../Assets/images/hero1.png'
-import HeroBg2 from '../../Assets/images/hero2.png'
+import HeroBg1 from "../../Assets/images/hero1.png";
+import HeroBg2 from "../../Assets/images/hero2.png";
 gsap.registerPlugin(ScrollTrigger);
 
 function Main() {
@@ -19,10 +19,10 @@ function Main() {
   const proButton = useRef();
   useLayoutEffect(() => {
     let mm = gsap.matchMedia();
-    let mysplittext = new SplitType(proHead.current, {type:"chars"});
+    let mysplittext = new SplitType(proHead.current, { type: "chars" });
     let chars = mysplittext.chars;
     mm.add(
-      '(min-width: 1000px)',
+      "(min-width: 1000px)",
       () => {
         var tl1 = gsap.timeline({
           scrollTrigger: {
@@ -114,7 +114,6 @@ function Main() {
           .to(
             ".product_image",
             {
-            
               top: "-100%",
               width: "28%",
               duration: 2.5,
@@ -124,7 +123,7 @@ function Main() {
           .to(
             ".name_left",
             {
-              left:"83%",
+              left: "83%",
               // rotate: "90deg",
               //  border:"2px solid white",
               fontSize: "8vw",
@@ -138,8 +137,8 @@ function Main() {
             ".name_right",
             {
               // left: "86.5%",
-              right:"20%",
-              textAlign:"right",
+              right: "20%",
+              textAlign: "right",
               // rotate: "90deg",
               // border:"2px solid white",
               fontSize: "8vw",
@@ -147,22 +146,22 @@ function Main() {
               top: "64%",
             },
             "b"
-          ).to(
+          )
+          .to(
             ".header",
             {
-              
               duration: 3,
               opacity: 1,
-             
             },
             "b"
-          ).to(
+          )
+          .to(
             ".empower",
             {
               // left: "86.5%",
-              position:"absolute",
-              right:"8%",
-              textAlign:"right",
+              position: "absolute",
+              right: "8%",
+              textAlign: "right",
               // rotate: "90deg",
               // border:"2px solid white",
               fontSize: "1.7vw",
@@ -232,211 +231,211 @@ function Main() {
       });
       // this setup code only runs when viewport is at least 800px wide
       tl1
-      .to(
-        ".top",
-        {
-          y: "-100vh",
-          duration: 1,
-        },
-        "a"
-      )
-      .to(
-        ".bottom",
-        {
-          y: "100vh",
-          duration: 1,
-        },
-        "a"
-      )
-      .to(
-        ".action_perform",
-        {
-          bottom: "-100%",
-          opacity: 0,
-          duration: 1.5,
-        },
-        "a"
-      )
-      .to(
-        ".center",
-        {
-          // backgroundImage:
-          //   "linear-gradient(90deg,rgba(0, 0, 0, 0.48), rgba(0, 0, 0, 1))",
-          duration: 1.5,
-        },
-        "a"
-      )
-      .to(
-        ".box_content",
-        {
-          opacity: 1,
-          scale: 1,
-          duration: 2,
-        },
-        "a"
-      )
-      .to(
-        ".product_image",
-        {
-          top: "30%",
-          duration: 2,
-        },
-        "a"
-      )
-      .to(
-        ".center",
-        {
-          // backgroundImage:
-          //   "linear-gradient(90deg,rgba(7, 7, 7, 0.8) 15%,rgba(20, 20, 20, 0.8) ,rgba(44, 44, 44, 0.6))",
-          duration: 2.5,
-        },
-        "b"
-      )
-      .to(
-        ".product_image",
-        {
-          top: "50%",
-          width: "33vmax",
-          duration: 2.5,
-        },
-        "b"
-      )
-      .to(
-        ".name_left",
-        {
-          
-          // rotate: "90deg",
-          //  border:"2px solid white",
-          fontSize: "8vw",
-          top: "24%",
-          duration: 3,
-          textShadow: " 15px 14px 2px rgba(0, 0, 0, 0.3)",
-        },
-        "b"
-      )
-      .to(
-        ".name_right",
-        {
-          // left: "-100%",
-        
-          textAlign:"right",
-          // rotate: "90deg",
-          // border:"2px solid white",
-          fontSize: "8vw",
-          duration: 3,
-          top: "24%",
-        },
-        "b"
-      ).to(
-        ".empower",
-        {
-          // left: "86.5%",
-          position:"absolute",
-          right:"8%",
-          textAlign:"right",
-          // rotate: "90deg",
-          // border:"2px solid white",
-          fontSize: "1.7vw",
-          duration: 3,
-          opacity: 1,
-          top: "78%",
-        },
-        "b"
-      ).to(
-        ".header",
-        {
-          
-          duration: 3,
-          opacity: 1,
-         
-        },
-        "b"
-      )
-      .to(
-        ".product_image",
-        {
-          top: "20%",
-          width: "29vmax",
-          duration: 1.1,
-          opacity:0
-        },
-        "c"
-      )
-      .to(
-        "h2",
-        {
-          duration: 1,
-          opacity:1
-        },
-        "c"
-      )
-      .to(
-        chars,
-        {
-          opacity: 1,
-          duration: 2,
-          stagger:0.3
-        },
-        "d"
-      )
-      .to(
-        ".line",
-        {
-          margin:"0px",
-          duration:2
-        },
-        "f"
-      )
-      .to(
-        "h2",
-        {
-          paddingLeft:"10px",
-          top:"6vmax",
-          duration:2
-        },
-        "e"
-      )
-      .to(
-        "._item-container-mainPage",{
-          top:"10vmax",
-          opacity:1,
-          duration:2.5,
-          height:"180vh"
-        },
-        "e"
-      )
-      .to(
-        ".para_first_center",
-        {
-          opacity: 1,
-          duration: 2.5,
-        },
-        "e"
-      )
-      .to(
-        ".para_second_center",
-        {
-          opacity: 1,
-          duration: 2.5,
-        },
-        "e"
-      )
-      .to(
-        ".center_product_price",
-        {
-          opacity: 1,
-          duration: 2.5
-        },
-        "e"
-      )
-      .to(
-        ".center_button",
-        {
-          opacity: 1,
-          duration: 2.5,
-        },
-        "e"
-      )
+        .to(
+          ".top",
+          {
+            y: "-100vh",
+            duration: 1,
+          },
+          "a"
+        )
+        .to(
+          ".bottom",
+          {
+            y: "100vh",
+            duration: 1,
+          },
+          "a"
+        )
+        .to(
+          ".action_perform",
+          {
+            bottom: "-100%",
+            opacity: 0,
+            duration: 1.5,
+          },
+          "a"
+        )
+        .to(
+          ".center",
+          {
+            // backgroundImage:
+            //   "linear-gradient(90deg,rgba(0, 0, 0, 0.48), rgba(0, 0, 0, 1))",
+            duration: 1.5,
+          },
+          "a"
+        )
+        .to(
+          ".box_content",
+          {
+            opacity: 1,
+            scale: 1,
+            duration: 2,
+          },
+          "a"
+        )
+        .to(
+          ".product_image",
+          {
+            top: "30%",
+            duration: 2,
+          },
+          "a"
+        )
+        .to(
+          ".center",
+          {
+            // backgroundImage:
+            //   "linear-gradient(90deg,rgba(7, 7, 7, 0.8) 15%,rgba(20, 20, 20, 0.8) ,rgba(44, 44, 44, 0.6))",
+            duration: 2.5,
+          },
+          "b"
+        )
+        .to(
+          ".product_image",
+          {
+            top: "50%",
+            width: "33vmax",
+            duration: 2.5,
+          },
+          "b"
+        )
+        .to(
+          ".name_left",
+          {
+            // rotate: "90deg",
+            //  border:"2px solid white",
+            fontSize: "8vw",
+            top: "24%",
+            duration: 3,
+            textShadow: " 15px 14px 2px rgba(0, 0, 0, 0.3)",
+          },
+          "b"
+        )
+        .to(
+          ".name_right",
+          {
+            // left: "-100%",
+
+            textAlign: "right",
+            // rotate: "90deg",
+            // border:"2px solid white",
+            fontSize: "8vw",
+            duration: 3,
+            top: "24%",
+          },
+          "b"
+        )
+        .to(
+          ".empower",
+          {
+            // left: "86.5%",
+            position: "absolute",
+            right: "8%",
+            textAlign: "right",
+            // rotate: "90deg",
+            // border:"2px solid white",
+            fontSize: "1.7vw",
+            duration: 3,
+            opacity: 1,
+            top: "78%",
+          },
+          "b"
+        )
+        .to(
+          ".header",
+          {
+            duration: 3,
+            opacity: 1,
+          },
+          "b"
+        )
+        .to(
+          ".product_image",
+          {
+            top: "20%",
+            width: "29vmax",
+            duration: 1.1,
+            opacity: 0,
+          },
+          "c"
+        )
+        .to(
+          "h2",
+          {
+            duration: 1,
+            opacity: 1,
+          },
+          "c"
+        )
+        .to(
+          chars,
+          {
+            opacity: 1,
+            duration: 2,
+            stagger: 0.3,
+          },
+          "d"
+        )
+        .to(
+          ".line",
+          {
+            margin: "0px",
+            duration: 2,
+          },
+          "f"
+        )
+        .to(
+          "h2",
+          {
+            paddingLeft: "10px",
+            top: "6vmax",
+            duration: 2,
+          },
+          "e"
+        )
+        .to(
+          "._item-container-mainPage",
+          {
+            top: "10vmax",
+            opacity: 1,
+            duration: 2.5,
+            height: "180vh",
+          },
+          "e"
+        )
+        .to(
+          ".para_first_center",
+          {
+            opacity: 1,
+            duration: 2.5,
+          },
+          "e"
+        )
+        .to(
+          ".para_second_center",
+          {
+            opacity: 1,
+            duration: 2.5,
+          },
+          "e"
+        )
+        .to(
+          ".center_product_price",
+          {
+            opacity: 1,
+            duration: 2.5,
+          },
+          "e"
+        )
+        .to(
+          ".center_button",
+          {
+            opacity: 1,
+            duration: 2.5,
+          },
+          "e"
+        );
       // .to(
       //   ".product_image",
       //   {
@@ -479,8 +478,6 @@ function Main() {
     return () => mm.revert();
   }, []);
 
-
-
   return (
     <>
       <div className="main_head" ref={mn}>
@@ -492,52 +489,32 @@ function Main() {
         <div className="center">
           <div className="box_content">
             <span className="name_left" id="evo_left">
-            Teific
+              Teific
             </span>
             <span className="name_right ">Technology</span>
-            <p className='lg:text-2xl  empower'>Empowering ease of your life</p>
+            <p className="lg:text-2xl  empower">Empowering ease of your life</p>
           </div>
           <div className="product_image">
             <img src={EVO} alt="" srcSet="" />
           </div>
           <div className="header">
-          <Header/>
+            <Header />
           </div>
-         
+
           <div className="center_main_content_left">
-            {/* <div className="content_center">
-              <h2 ref={proHead}>EVO EV CHARGER</h2>
-              <div className="_item-container-mainPage">
-              <p className="para_first_center" ref={paraFirst}>
-                Teific UPI Supported Hassle Free EV Charger
-              </p>
-              <p className="para_second_center" ref={paraSec}>
-                EVOCharger..Say goodbye to apps and payment hassles. Enjoy
-                seamless charging, effortless transactions, and a greener
-                journey today.
-              </p>
-              <p className="center_product_price" ref={proPrice}>₹19,999</p>
-              <div className="center_button" ref={proButton}>
-                <button className="center_main_button buy_button">
-                  Add To Cart
-                </button>
-                <button className="center_main_button info_button">
-                  Know More
-                </button>
-                </div>
-              </div>
-            </div> */}
-           
-
-
-
-            <div className='relative left-0 w-full z-10 h-full top-0'>
+            <div className="relative left-0 w-full z-10 h-full top-0">
               {/* <img src={HeroBg1} alt='bg-hero' className='bg-cover w-full top-3 md:top-4 -left-[0.2rem] lg:top-[2.2rem] lg:-left-[0.35rem] image light '/> */}
-                   <img src={HeroBg1} alt='bg-hero' className='bg-cover w-full  image light '/>
-               <img src={HeroBg2} alt='bg-hero' className='bg-cover w-full  no-light '/>
-              </div>
-
-
+              <img
+                src={HeroBg1}
+                alt="bg-hero"
+                className="bg-cover w-full h-screen  image light "
+              />
+              <img
+                src={HeroBg2}
+                alt="bg-hero"
+                className="bg-cover w-full h-screen  no-light "
+              />
+            </div>
           </div>
         </div>
         <div className="bottom">
