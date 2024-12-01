a03ec1f56d5502eb2bd335b2c3a39a6a60ea3127
Varun Gupta <varungupta1228@gmail.com>
2024-12-01 19:23:24 +0530

    added more func

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
   whileInView="animate"
   exit="exit"
   viewport={{ once:true, amount: 0.9 }} // Ensures it animates only once when 50% is in view
-  variants={slideAnimation('right')} className='z-20 absolute  top-[20%] lg:top-[40%] lg:right-36 text-center w-full lg:text-right'>
+  variants={slideAnimation('up')} className='z-20 absolute  top-[20%] lg:top-[40%] lg:right-36 text-center w-full lg:text-right'>
         <h2 className='absolute text-4xl top-0 right-[20%] lg:right-0  lg:text-8xl text-[#FBC93E] bg-[#FBC93E] bg-clip-text blur-xl   shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]'>Teific <br className='hidden lg:block'/> Technology</h2>
         <h2 className='relative top-0  text-4xl lg:text-8xl text-[#FBC93E] bg-[#FBC93E] bg-clip-text font-bold '>Teific <br className='hidden lg:block'/> Technology</h2>
        <p className='lg:text-2xl mt-5'>Empowering ease of your life</p>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,10 @@
-import React, { useState, useEffect, useRef, useLayoutEffect } from "react";
-import Header from "../components/Header";
-import Hero from "../components/Hero/Hero.jsx";
-import Template from "../utils/Template";
-import { HomePageContent } from "../static/data";
-import Footer from "../components/Footer";
-import Main from "../components/Main/Main";
-import gsap from "gsap";
-import SplitType from "split-type";
-import "./main.css";
-import { ReactComponent as Mouse } from "../Assets/images/magic-mouse_1.svg";
+import React, { useState, useEffect } from 'react';
+import Header from '../components/Header';
+import Hero from '../components/Hero/Hero.jsx';
+import Template from '../utils/Template';
+import { HomePageContent } from '../static/data';
+import Footer from '../components/Footer';
+import Main from '../components/Main/Main';
 
 const HomePage = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -19,134 +15,37 @@ const HomePage = () => {
     };
 
     handleResize(); // Initial check
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
 
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const mn = useRef();
-  const proHead = useRef();
-
-  useLayoutEffect(() => {
-    let mm = gsap.matchMedia();
-    let mysplittext = new SplitType(proHead.current, { type: "chars" });
-    let chars = mysplittext.chars;
-
-    mm.add("(max-width: 1000px)", () => {
-      var tl1 = gsap.timeline({
-        scrollTrigger: {
-          trigger: mn.current,
-          start: "50% 50%",
-          end: "55% 50%",
-          scrub: 2,
-          pin: true,
-        },
-      });
-      // this setup code only runs when viewport is at least 800px wide
-      tl1
-        .to(
-          ".top",
-          {
-            y: "-100vh",
-            duration: 1,
-          },
-          "a"
-        )
-        .to(
-          ".bottom",
-          {
-            y: "100vh",
-            duration: 1,
-          },
-          "a"
-        )
-        .to(
-          ".action_perform",
-          {
-            bottom: "-100%",
-            opacity: 0,
-            duration: 1.5,
-          },
-          "a"
-        )
-        .to(
-          ".header",
-          {
-            duration: 0.1,
-            opacity: 1,
-            zIndex: 100000,
-          },
-          "b"
-        );
-    });
-    return () => mm.revert();
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   return (
-    <>
-   
-    {
-      isMobile &&  
-      <div>
-        <div className="main_head" ref={mn}>
-          <div className="top">
-            <h1 id="head_top_heading">
-              Te<span>i</span>fic
-            </h1>
-          </div>
-  
-          <div className="bottom">
-            <h1 id="head_bottom_heading">
-              Te<span>i</span>fic
-            </h1>
-          </div>
-          <div className="action_perform">
-            <Mouse className="mouse"> This</Mouse>
-            <p>Scroll Down</p>
-          </div>
+
+    
+    <div className="overflow-x-hidden">
+      {isMobile && (
+        <div className="mobile-size">
+          <Header />
+          <Hero />
         </div>
-  
-        <div className="overflow-x-hidden absolute top-12 ">
-         
-            <div className="mobile-size">
-              <Hero />
-            </div>
-        
-  
-          <div className="header ">
-            <Header />
-          </div>
-          {HomePageContent.map((item) => (
-            <Template
-              key={item.id}
-              id={item.id}
-              bgText={item.bgText}
-              heading={item.heading}
-              content={item.content}
-              imageUrl={item.imageUrl}
-            />
-          ))}
-          <Footer />
-        </div>
-      </div>
-    }
-    {
-   !isMobile && <div className="overflow-x-hidden">
-         <Main/>
-          {HomePageContent.map((item) => (
-            <Template
-              key={item.id}
-              id={item.id}
-              bgText={item.bgText}
-              heading={item.heading}
-              content={item.content}
-              imageUrl={item.imageUrl}
-            />
-          ))}
-          <Footer />
-   </div>
-    }
-    </>
+      )}
+      {
+        !isMobile &&  <Main />
+      }
+     
+      {HomePageContent.map((item) => (
+        <Template
+          key={item.id}
+          id={item.id}
+          bgText={item.bgText}
+          heading={item.heading}
+          content={item.content}
+          imageUrl={item.imageUrl}
+        />
+      ))}
+      <Footer />
+    </div>
   );
 };
 
